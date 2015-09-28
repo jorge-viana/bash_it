cb5896b94744729653a2eb161606e144c748ecac
jm <jorge-viana@users.noreply.github.com>
2015-09-29 00:50:54 +0200

    adding constants, declaring variables

--- test.sh
+++ test.sh
@@ -1,107 +1,121 @@
 #!/bin/bash
 
+# todo
+# make it more modular
+# eventually change the name of the script
+# 
+# comment tests, @ignore, todos
+
+
 set -u
 
-suite_files=$(ls test/*_test.sh)
+readonly _MILLI_TO_NANO_FACTOR=$(( 10 ** 6 ))
 
-global_start_time=0
-global_end_time=0
-global_total_time_milli=0
+declare _suite_file=""
+declare _suite_files=""
 
-global_number_tests=0
-global_number_failed_tests=0
+declare -i _global_start_time=0
+declare -i _global_end_time=0
+declare -i _global_total_time_milli=0
 
-global_failed_tests=""
+declare -i _global_number_tests=0
+declare -i _global_number_failed_tests=0
+
+declare _global_failed_tests=""
 
 
-global_start_time=$(date +%s%N)
+_suite_files=$(ls test/*_test.sh)
+_global_start_time=$(date +%s%N)
 
 
-for suite_file in $suite_files
+for _suite_file in $_suite_files
 do
 
-  suite_nbr_tests=0
-  suite_nbr_failed_tests=0
-  suite_start=0
-  suite_end=0
-  suite_duration_milli=0
-  suite_failed_tests=""
+  declare _test=""
+  declare _test_suite=""
+  declare -i _suite_nbr_tests=0
+  declare -i _suite_nbr_failed_tests=0
+  declare -i _suite_start=0
+  declare -i _suite_end=0
+  declare -i _suite_duration_milli=0
+  declare _suite_failed_tests=""
 
   echo ""
-  echo "Running $(basename ${suite_file})"
+  echo "Running $(basename ${_suite_file})"
 
   # import (source) suite file
-  . $suite_file
+  . $_suite_file
 
-  test_suite=$(sed -n -e '/#@test/,/function/ p' $suite_file | grep function | awk '{print $2}')
+  _test_suite=$(sed -n -e '/#@test/,/function/ p' $_suite_file | grep function | awk '{print $2}')
 
 
-  suite_start=$(date +%s%N)
+  _suite_start=$(date +%s%N)
 
-  for test in $test_suite
+
+  for _test in $_test_suite
   do
 
-    test_start=0
-    test_end=0
-    test_duration_milli=0
+    declare -i _test_start=0
+    declare -i _test_end=0
+    declare -i _test_duration_milli=0
+    declare -i _test_result=0
 
-    suite_nbr_tests=$(( suite_nbr_tests + 1 ))
+    _suite_nbr_tests+=1
 
-    test_start=$(date +%s%N)
+    _test_start=$(date +%s%N)
 
-    $("$test")
-    test_result=$?
+    $("$_test")
+    _test_result=$?
 
-    test_end=$(date +%s%N)
-    test_duration_milli=$(( (test_end - test_start) / 1000000 ))
+    _test_end=$(date +%s%N)
+    _test_duration_milli=$(( (_test_end - _test_start) / _MILLI_TO_NANO_FACTOR ))
 
 
-    if [ $test_result -ne 0 ]
+    if [ $_test_result -ne 0 ]
     then
-      echo "  ${test}, took ${test_duration_milli} milliseconds  <<< FAILED"
-      suite_nbr_failed_tests=$(( suite_nbr_failed_tests + 1 ))
-      suite_failed_tests+="$( basename ${suite_file}) - ${test}\n"
+      echo "  ${_test}, took ${_test_duration_milli} milliseconds  <<< FAILED"
+      _suite_nbr_failed_tests+=1
+      _suite_failed_tests+="$( basename ${_suite_file}) - ${_test}\n"
     fi
   done
 
-  suite_end=$(date +%s%N)
-  suite_duration_milli=$(( (suite_end - suite_start) / 1000000 ))
+  _suite_end=$(date +%s%N)
+  _suite_duration_milli=$(( (_suite_end - _suite_start) / _MILLI_TO_NANO_FACTOR ))
 
-  global_number_tests=$(( global_number_tests + suite_nbr_tests ))
-  global_number_failed_tests=$(( global_number_failed_tests + suite_nbr_failed_tests ))
+  _global_number_tests+=_suite_nbr_tests
+  _global_number_failed_tests+=_suite_nbr_failed_tests
 
-  global_failed_tests+="${suite_failed_tests}"
+  _global_failed_tests+="${_suite_failed_tests}"
 
   #suite stats
-  if [ "${suite_failed_tests}" != "" ]
+  echo -n "  Tests run: ${_suite_nbr_tests}, Failures: ${_suite_nbr_failed_tests}, Time elapsed: ${_suite_duration_milli} milliseconds"
+  if [ "${_suite_failed_tests}" != "" ]
   then
-    echo "  Tests run: ${suite_nbr_tests}, Failures: ${suite_nbr_failed_tests}, Time elapsed: ${suite_duration_milli} milliseconds  <<< FAILURE!"
-  else
-    echo "  Tests run: ${suite_nbr_tests}, Failures: ${suite_nbr_failed_tests}, Time elapsed: ${suite_duration_milli} milliseconds"
+    echo "  <<< FAILURE!"
   fi
   
 done
 
-global_end_time=$(date +%s%N)
-global_total_time_milli=$(( (global_end_time - global_start_time) / 1000000 ))
+_global_end_time=$(date +%s%N)
+_global_total_time_milli=$(( (_global_end_time - _global_start_time) / _MILLI_TO_NANO_FACTOR ))
 
 echo ""
 echo "----------------------------------------------------------------------"
 echo "Results:"
 echo ""
 
-if [ "${global_failed_tests}" != "" ]
+if [ "${_global_failed_tests}" != "" ]
 then
   echo "Failed tests:"
-  echo -e "${global_failed_tests}"
+  echo -e "${_global_failed_tests}"
 fi
 
-echo "Tests run: ${global_number_tests}, Failures: ${global_number_failed_tests}"
-echo "Total time: ${global_total_time_milli} milliseconds"
+echo "Tests run: ${_global_number_tests}, Failures: ${_global_number_failed_tests}"
+echo "Total time: ${_global_total_time_milli} milliseconds"
 echo "Finished at: $(date)"
 echo "----------------------------------------------------------------------"
 
-if [ $global_number_failed_tests -ne 0 ]
+if [ $_global_number_failed_tests -ne 0 ]
 then
   exit 1
 fi
